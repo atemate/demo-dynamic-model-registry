d8f9c3640919840f69925783f01904c4646a9943
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2022-10-18 23:10:11 +0200

    Fix requirements

--- mlflow_toolkit/setup.py
+++ mlflow_toolkit/setup.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+REQUIREMENTS_TXT = Path(__file__).parent / "requirements.txt"
+REQUIREMENTS = REQUIREMENTS_TXT.read_text().splitlines()
+
 
 setup(
     name="mlflow_toolkit",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["mlflow", "sklearn", "pandas"],
+    install_requires=REQUIREMENTS,
 )
--- model_package/setup.py
+++ model_package/setup.py
@@ -1,12 +1,14 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+REQUIREMENTS_TXT = Path(__file__).parent / "requirements.txt"
+REQUIREMENTS = REQUIREMENTS_TXT.read_text().splitlines()
+
 
 setup(
     name="mlflow_toolkit",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=[
-        "mlflow==1.29.0",
-        "sklearn",  # for mlflow.sklearn.log_model
-        "joblib",
-    ],
+    install_requires=REQUIREMENTS,
 )
