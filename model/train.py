e66306cf2c9aa1312ff8578d972d530c7e14d9ec
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2022-10-18 22:43:06 +0200

    Set fallback

--- model/train.py
+++ model/train.py
@@ -49,17 +49,14 @@ class MockModel:
 
 
 def _get_or_create_mlflow_experiment_id(exp_name: str, use_legacy_api=True) -> str:
+    """MLflow helper to get or create an experiment by name."""
     if use_legacy_api:
+        # Legacy API: DagsHub does not support search_experiments()
         exps = [e for e in mlflow.list_experiments() if e.name == exp_name]
     else:
         exps = mlflow.search_experiments(filter_string=f"name='{exp_name}'")
 
-    if exps:
-        exp_id = exps[0].experiment_id
-    else:
-        exp_id = mlflow.create_experiment(exp_name)
-
-    return exp_id
+    return exps[0].experiment_id if exps else mlflow.create_experiment(exp_name)
 
 
 def train(
