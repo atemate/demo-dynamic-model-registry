a1bd2896cc561f1a108630347be8026846bc2dc4
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2022-10-18 20:30:58 +0200

    Move to model

--- train-with-different-params.sh
+++ train-with-different-params.sh
@@ -9,6 +9,7 @@ for learning_rate in 0.01 0.05 0.1 0.3 0.5; do
         echo max_depth=$max_depth
         for n_estimators in 5 10 20 50; do
             echo n_estimators=$n_estimators
+            
             python train.py \
                 --experiment_name="train-$TIMESTAMP" \
                 --run_name="lr-$learning_rate-md-$max_depth-ne-$n_estimators" \
@@ -16,7 +17,7 @@ for learning_rate in 0.01 0.05 0.1 0.3 0.5; do
                 --max_depth $max_depth \
                 --n_estimators $n_estimators \
                 --timestamp $TIMESTAMP
-            echo "Done"
+
         done
     done
 done
