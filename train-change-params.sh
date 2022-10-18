913aeede2f8b6abd72fe294dc1eb4ff40ee08f9f
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2022-10-18 20:36:28 +0200

    Fix paths

--- train-change-params.sh
+++ train-change-params.sh
@@ -10,7 +10,7 @@ for learning_rate in 0.01 0.05 0.1 0.3 0.5; do
         for n_estimators in 5 10 20 50; do
             echo n_estimators=$n_estimators
             
-            python train.py \
+            python model/train.py \
                 --experiment_name="train-change-params-$TIMESTAMP" \
                 --run_name="lr-$learning_rate-md-$max_depth-ne-$n_estimators" \
                 --learning_rate $learning_rate \
